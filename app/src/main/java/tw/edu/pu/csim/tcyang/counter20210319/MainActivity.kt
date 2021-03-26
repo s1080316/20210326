cc8a12abb3c3bf9733e02fecc96615e5e8a88018
student <s10803161@gm.pu.edu.tw>
2021-03-26 15:23:52 +0800

    實際設定監聽及觸發方法

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
@@ -18,8 +18,14 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
         btn.setOnClickListener(this)
         btn2.setOnClickListener(this)
         btn3.setOnClickListener(this)
-    }
 
+        btnHappy.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                counter= (1..100).random()
+                txv.text = counter.toString()
+            }
+        })
+    }
 
     fun Add(v:View){
         if (v == btn){

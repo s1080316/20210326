37436ae91f02a76ef01f2052f4ecd3b2319d1b03
student <s10803161@gm.pu.edu.tw>
2021-03-26 15:41:23 +0800

    實際設定監聽及觸發方法

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
@@ -5,7 +5,8 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() ,View.OnClickListener{
+class MainActivity : AppCompatActivity() ,
+    View.OnClickListener,View.OnLongClickListener{
 
     var counter:Int = 0
 
@@ -48,6 +49,12 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
             counter=0
         }
         txv.text = counter.toString()
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        counter += 2
+        txv.text=counter.toString()
+        return true
     }
 
 
